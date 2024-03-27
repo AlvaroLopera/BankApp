9b947f5fb3199a97e68a5a6b18c73257fac0e973
Alvaro Lopera <loperamendizabalalvaro@gmail.com>
2024-03-27 09:44:57 +0100

    JPA Entity update

--- app/src/main/java/com/example/app/entity/TransactionEntity.java
+++ app/src/main/java/com/example/app/entity/TransactionEntity.java
@@ -1,5 +1,89 @@
 package com.example.app.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class TransactionEntity {
-    
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private double amount;
+	
+	@Enumerated(EnumType.STRING)
+	private TransactionTypeEntity transactionType;
+	
+	private Date transaction_date;
+
+	@ManyToOne
+	@JoinColumn(name = "source_account_id")
+	private AccountEntity sourceAccount;
+
+	@ManyToOne
+	@JoinColumn(name = "target_account_id")
+	private AccountEntity targetAccount;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	public TransactionTypeEntity getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(TransactionTypeEntity transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public Date getTransaction_date() {
+		return transaction_date;
+	}
+
+	public void setTransaction_date(Date transaction_date) {
+		this.transaction_date = transaction_date;
+	}
+
+	public AccountEntity getSourceAccount() {
+		return sourceAccount;
+	}
+
+	public void setSourceAccount(AccountEntity sourceAccount) {
+		this.sourceAccount = sourceAccount;
+	}
+
+	public AccountEntity getTargetAccount() {
+		return targetAccount;
+	}
+
+	public void setTargetAccount(AccountEntity targetAccount) {
+		this.targetAccount = targetAccount;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction [id=" + id + ", amount=" + amount + ", transactionType=" + transactionType
+				+ ", transaction_date=" + transaction_date + ", sourceAccount=" + sourceAccount + ", targetAccount="
+				+ targetAccount + "]";
+	}
 }
--- app/src/main/java/com/example/app/entity/UserEntity.java
+++ app/src/main/java/com/example/app/entity/UserEntity.java
@@ -1,5 +1,108 @@
 package com.example.app.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class UserEntity {
     
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String password;
+
+	@Column(unique = true, nullable = false )
+	private String email;
+	private String address;
+	private String phone_number;
+	private int otpRetryCount;
+	private LocalDateTime lastOtpRequestTime;
+
+	// Establishing a one-to-one relationship with the account
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	private AccountEntity account;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public AccountEntity getAccount() {
+		return account;
+	}
+
+	// Convenience method to set the user's account
+	public void setAccount(AccountEntity account) {
+		this.account = account;
+		account.setUser(this);
+	}
+
+	public int getOtpRetryCount() {
+		return otpRetryCount;
+	}
+
+	public void setOtpRetryCount(int otpRetryCount) {
+		this.otpRetryCount = otpRetryCount;
+	}
+
+	public LocalDateTime getLastOtpRequestTime() {
+		return lastOtpRequestTime;
+	}
+
+	public void setLastOtpRequestTime(LocalDateTime lastOtpRequestTime) {
+		this.lastOtpRequestTime = lastOtpRequestTime;
+	}
+
+
 }

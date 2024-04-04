901427032d3853ccf570e0152f595e6d768d30bd
Alvaro Lopera <loperamendizabalalvaro@gmail.com>
2024-04-04 10:50:24 +0200

    DTP Files updated

--- app/src/main/java/com/example/app/dto/TransactionDTO.java
+++ app/src/main/java/com/example/app/dto/TransactionDTO.java
@@ -1,5 +1,52 @@
 package com.example.app.dto;
 
-public class TransactionDTO {
+import java.util.Date;
 
+import com.example.app.entity.TransactionTypeEntity;
+
+public class TransactionDTO {
+    private Long id; 
+    private double amount;
+    private TransactionTypeEntity transaction_type;
+    private Date transaction_date;
+    private String sourceAccountNumber;
+    private String targetAccountNumber;
+    
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public TransactionType getTransaction_type() {
+		return transaction_type;
+	}
+	public void setTransaction_type(TransactionType transaction_type) {
+		this.transaction_type = transaction_type;
+	}
+	public Date getTransaction_date() {
+		return transaction_date;
+	}
+	public void setTransaction_date(Date transaction_date) {
+		this.transaction_date = transaction_date;
+	}
+	public String getSourceAccountNumber() {
+		return sourceAccountNumber;
+	}
+	public void setSourceAccountNumber(String sourceAccountNumber) {
+		this.sourceAccountNumber = sourceAccountNumber;
+	}
+	public String getTargetAccountNumber() {
+		return targetAccountNumber;
+	}
+	public void setTargetAccountNumber(String targetAccountNumber) {
+		this.targetAccountNumber = targetAccountNumber;
+	}
 }
